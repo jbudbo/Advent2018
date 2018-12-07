0e233d5e2a16c235e3b6535fe1f03288049bed7e
Morris, Justin <Justin.Morris@allscripts.com>
2018-12-07 09:38:55 -0500

    Day 2 Part 1 success

---
+++ Advent/Day2.fs
@@ -0,0 +1,24 @@
+﻿module Day2
+
+let private makePath file = sprintf "D:\AoC\Advent2018\%s.txt" file
+
+let private dataLines file =
+    System.IO.File.ReadAllLines (makePath file)
+
+let private processString (chars:string) =
+    chars.ToCharArray()
+    |> Array.countBy id
+    |> Array.where (fun (_,x) -> x > 1)
+    |> Array.unzip
+    |> snd
+    |> Array.distinct
+
+let part1 fileName =
+    let x = 
+        dataLines fileName
+        |> Array.map (fun s -> processString s)
+        |> Array.concat
+        |> Array.countBy id
+        |> Array.reduce (fun (_,x) (_,y) -> (x, y))
+     
+    (fst x) * (snd x)
--- Advent/Program.fs
+++ Advent/Program.fs
@@ -1,13 +1,15 @@
-﻿ // Learn more about F# at http://fsharp.org
+﻿// Learn more about F# at http://fsharp.org
 
 open System
 
 [<EntryPoint>]
 let main argv =
-    printfn "Day 1 solution %i" (Day1.part1 "day1")
+    printfn "Day 1 Part 1 solution %i" (Day1.part1 "day1")
 
-    let solution =
-        Day1.part2 "day1"
+    // TODO
+    //let solution = Day1.part2 "day1"
+
+    printfn "Day 2 Part 1 solution %i" (Day2.part1 "day2")
 
     Console.Read() |> ignore
 
